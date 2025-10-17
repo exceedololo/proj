e72170b5f45b65a504db37e3c7cca7fe20dda0e3
Alexandr Zhdanov <git.user.AlAlAZHdanov@inno.tech>
2025-10-17 15:38:12 +0300

    ma

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,25 +6,15 @@ public class Main {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-        System.out.println(7);
-        System.out.println(8);
-        System.out.println(8);
-        System.out.println(134);
-        System.out.println(134);
-        System.out.println(134);
-        System.out.println(134);
-        System.out.println(134);
-        System.out.println(134);
-        System.out.println(666);
+        System.out.println("Hello World");
+        System.out.println("1001");
+
         //guiiouio
     }
 
     public static void doSomething(){
-        System.out.println("doSomething");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
